cd555b351daa632fc92254008645456f2de56e84
zeb <zebhall@gmail.com>
2022-12-27 19:05:29 +0800

    v0.0.1
    initial

---
+++ spectral-anomaly-checker.py
@@ -0,0 +1,79 @@
+# XRF Spectral Anomaly Checker by ZH
+versionNum = 'v0.0.1'
+versionDate = '2022/12/27'
+
+import xraydb as xrdb
+import pandas as pd
+
+
+def elementZtoSymbol(Z):        # Returns 1-2 character Element symbol as a string
+    if Z <= 118:
+        elementSymbols = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+        return elementSymbols[Z-1]
+    else:
+        return 'Error: Z out of range'
+
+def elementZtoSymbolZ(Z):       # Returns 1-2 character Element symbol formatted WITH atomic number in brackets
+    if Z <= 118:
+        elementSymbols = ['H (1)', 'He (2)', 'Li (3)', 'Be (4)', 'B (5)', 'C (6)', 'N (7)', 'O (8)', 'F (9)', 'Ne (10)', 'Na (11)', 'Mg (12)', 'Al (13)', 'Si (14)', 'P (15)', 'S (16)', 'Cl (17)', 'Ar (18)', 'K (19)', 'Ca (20)', 'Sc (21)', 'Ti (22)', 'V (23)', 'Cr (24)', 'Mn (25)', 'Fe (26)', 'Co (27)', 'Ni (28)', 'Cu (29)', 'Zn (30)', 'Ga (31)', 'Ge (32)', 'As (33)', 'Se (34)', 'Br (35)', 'Kr (36)', 'Rb (37)', 'Sr (38)', 'Y (39)', 'Zr (40)', 'Nb (41)', 'Mo (42)', 'Tc (43)', 'Ru (44)', 'Rh (45)', 'Pd (46)', 'Ag (47)', 'Cd (48)', 'In (49)', 'Sn (50)', 'Sb (51)', 'Te (52)', 'I (53)', 'Xe (54)', 'Cs (55)', 'Ba (56)', 'La (57)', 'Ce (58)', 'Pr (59)', 'Nd (60)', 'Pm (61)', 'Sm (62)', 'Eu (63)', 'Gd (64)', 'Tb (65)', 'Dy (66)', 'Ho (67)', 'Er (68)', 'Tm (69)', 'Yb (70)', 'Lu (71)', 'Hf (72)', 'Ta (73)', 'W (74)', 'Re (75)', 'Os (76)', 'Ir (77)', 'Pt (78)', 'Au (79)', 'Hg (80)', 'Tl (81)', 'Pb (82)', 'Bi (83)', 'Po (84)', 'At (85)', 'Rn (86)', 'Fr (87)', 'Ra (88)', 'Ac (89)', 'Th (90)', 'Pa (91)', 'U (92)', 'Np (93)', 'Pu (94)', 'Am (95)', 'Cm (96)', 'Bk (97)', 'Cf (98)', 'Es (99)', 'Fm (100)', 'Md (101)', 'No (102)', 'Lr (103)', 'Rf (104)', 'Db (105)', 'Sg (106)', 'Bh (107)', 'Hs (108)', 'Mt (109)', 'Ds (110)', 'Rg (111)', 'Cn (112)', 'Nh (113)', 'Fl (114)', 'Mc (115)', 'Lv (116)', 'Ts (117)', 'Og (118)']
+        return elementSymbols[Z-1]
+    else:
+        return 'Error: Z out of range'
+
+def elementZtoName(Z):          # Returns Element name 
+    if Z <= 118:
+        elementNames = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon', 'Nitrogen', 'Oxygen', 'Fluorine', 'Neon', 'Sodium', 'Magnesium', 'Aluminium', 'Silicon', 'Phosphorus', 'Sulfur', 'Chlorine', 'Argon', 'Potassium', 'Calcium', 'Scandium', 'Titanium', 'Vanadium', 'Chromium', 'Manganese', 'Iron', 'Cobalt', 'Nickel', 'Copper', 'Zinc', 'Gallium', 'Germanium', 'Arsenic', 'Selenium', 'Bromine', 'Krypton', 'Rubidium', 'Strontium', 'Yttrium', 'Zirconium', 'Niobium', 'Molybdenum', 'Technetium', 'Ruthenium', 'Rhodium', 'Palladium', 'Silver', 'Cadmium', 'Indium', 'Tin', 'Antimony', 'Tellurium', 'Iodine', 'Xenon', 'Caesium', 'Barium', 'Lanthanum', 'Cerium', 'Praseodymium', 'Neodymium', 'Promethium', 'Samarium', 'Europium', 'Gadolinium', 'Terbium', 'Dysprosium', 'Holmium', 'Erbium', 'Thulium', 'Ytterbium', 'Lutetium', 'Hafnium', 'Tantalum', 'Tungsten', 'Rhenium', 'Osmium', 'Iridium', 'Platinum', 'Gold', 'Mercury', 'Thallium', 'Lead', 'Bismuth', 'Polonium', 'Astatine', 'Radon', 'Francium', 'Radium', 'Actinium', 'Thorium', 'Protactinium', 'Uranium', 'Neptunium', 'Plutonium', 'Americium', 'Curium', 'Berkelium', 'Californium', 'Einsteinium', 'Fermium', 'Mendelevium', 'Nobelium', 'Lawrencium', 'Rutherfordium', 'Dubnium', 'Seaborgium', 'Bohrium', 'Hassium', 'Meitnerium', 'Darmstadtium', 'Roentgenium', 'Copernicium', 'Nihonium', 'Flerovium', 'Moscovium', 'Livermorium', 'Tennessine', 'Oganesson']
+        return elementNames[Z-1]
+    else:
+        return 'Error: Z out of range'
+
+
+def lineEnergiesToCSV(element):
+    anomalies = pd.DataFrame(data={'Element':[],'Line':[],'Type':[],'Energy':[]})
+    line_type_whitelist = ['Ka1','Ka2','Ka3','Ka4','Ka5','Kb1','Kb2','Kb3','Kb4','Kb5','La1','La2','La3','La4','La5','Lb1','Lb2','Lb3','Lb4','Lb5']
+    # Iterate through all Elements
+    for z in range(1,104):
+            element = elementZtoSymbol(z)
+            # Get standard emission lines for all elements
+            try:
+                for name, line in xrdb.xray_lines(element).items():
+                    energy = line.energy
+
+                    if name in line_type_whitelist:
+                        if energy <= 50000:
+                            newrow = {'Element':element,'Line':name,'Type':'Emission Line','Energy':energy}
+                            anomalies = anomalies.append(newrow, ignore_index = True)
+                            if energy*2 <= 50000:   # check if sum peak is possible for standard detector (50kV)
+                                newrow2 = {'Element':element,'Line':name,'Type':'Sum Peak (2x)','Energy':(energy*2)}
+                                anomalies = anomalies.append(newrow2, ignore_index = True)
+                            # if energy*3 <= 50000:   # check if 3x sum peak is possible for standard detector (50kV)
+                            #     newrow2 = {'Element':element,'Line':name,'Type':'Sum Peak (3x)','Energy':(energy*3)}
+                            #     anomalies = anomalies.append(newrow2, ignore_index = True)
+                            if energy-1700 > 0:     # check if escape peak is possible
+                                newrow4 = {'Element':element,'Line':name,'Type':'Escape Peak (Si)','Energy':(energy-1700)}
+                                anomalies = anomalies.append(newrow4, ignore_index = True)
+            except: 
+                print(f'Error getting lines for {element}')
+                pass
+
+    anomalies.sort_values(by='Energy', inplace=True)
+    anomalies.to_csv('all_lines.csv', index = False)
+    print(anomalies)
+
+def getClosestByEnergy(energy):
+    closest = energies_df.set_index('Energy').sub(energy).abs().idxmin()
+    return(closest)
+
+
+def main():
+    global energies_df
+    #lineEnergiesToCSV()
+    energies_df = pd.read_csv('energies.csv')
+    print(energies_df)
+    request = input('Enter energy of suspected anomaly (eV): ')
+    request = float(request)
+    print(getClosestByEnergy(request))
+
+
+if __name__ == main():
+    main()
